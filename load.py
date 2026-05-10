c60a9e67cea93c11242857b645161911fead65f1
theyaya <panilag.althea123@gmail.com>
2026-05-10 11:01:16 +0800

    Update load.py

--- load.py
+++ load.py
@@ -1,17 +1,24 @@
 import pandas as pd
 from database import engine
+import os
+from flask import Flask
+
 
 print("Starting Final Load...")
-
-
 df_j_clean = pd.read_sql('trf_japan', engine)
 df_m_clean = pd.read_sql('trf_myanmar', engine)
-
-
 df_final = pd.concat([df_j_clean, df_m_clean], ignore_index=True)
-
-
-print("Uploading to Presentation layer (Master Table)...")
 df_final.to_sql('fact_global_sales', engine, if_exists='replace', index=False)
+print("ETL Process Complete.")
 
-print("Load Stage: Success! Your Master Table 'fact_global_sales' is ready.")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>ETL Pipeline Live</h1><p>The Python script has successfully executed in the cloud. Presentation layer is ready.</p>"
+
+if __name__ == "__main__":
+    # Render uses the 'PORT' environment variable
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
